a3d4c91e59625be2894251f0f80fb3eef5aa4cbc
T44M <75268260+T44M@users.noreply.github.com>
2021-03-13 21:08:48 +0100

    changed Xpath

--- total.py
+++ total.py
@@ -2,45 +2,54 @@ import sys
 import datetime
 import requests
 import tweepy
-import os
+import time
 from lxml import html
-from os import environ
+
+response = requests.get('https://news.google.com/covid19/map?hl=ja&gl=JP&ceid=JP%3Aja&mid=%2Fm%2F03_3d&state=4')
+tag = html.fromstring(response.content)
 
 now = datetime.datetime.now()
 date = now.strftime("%Y/%m/%d %H:%M")
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_KEY_SECRET = environ['ACCESS_KEY_SECRET']
+def news_covid():
+    a, b, c, d = tag.xpath('//div[@class="UvMayb"]/text()')
+    a1, b1, c1, d1 = tag.xpath('//div[@class="tIUMlb"]/strong/text()')
+    tweet_world = f'''【新型コロナウィルス感染情報】
+日本国内
 
-def total_covid():
-    response = requests.get('https://www.worldometers.info/coronavirus/')
-    doc = html.fromstring(response.content)
-    total, deaths, recovered = doc.xpath('//div[@class="maincounter-number"]/span/text()')
+新たな感染者(累計感染者)：
+{a1}({a})
 
-    responsejp = requests.get('https://www.worldometers.info/coronavirus/country/japan/')
-    docjp = html.fromstring(responsejp.content)
-    totalj, deathsj, recoveredj = docjp.xpath('//div[@class="maincounter-number"]/span/text()')
+昨日の死亡者(累計死亡者):
+{b1}({b})
 
-    tweet_world = f'''【新型コロナ感染者数】
-日本
-   -感染者: {totalj}
-   -回復者: {recoveredj}
-   -死亡者: {deathsj}
-世界
-   -全世界累計: {total}
-   -全回復累計: {recovered}
-   -全死亡累計: {deaths} 
-ソース: https://bit.ly/2SIVKY6
+ソース: https://ux.nu/uOa8V
 #COVID19
 取得 {date}'''
-            
+        
     return tweet_world
 
+def vaccine_covid():
+    a, b, c, d = tag.xpath('//div[@class="UvMayb"]/text()')
+    a1, b1, c1, d1 = tag.xpath('//div[@class="tIUMlb"]/strong/text()')
+    tweet_world2 = f'''【新型コロナワクチン接種】
+日本国内
+
+昨日の接種数: {c1}
+合計接種数: {c}
+
+必要回数接種済み: {d}
+人口比: {d1}
+
+ソース: https://ux.nu/uOa8V
+#COVID19
+取得 {date}'''
+
+    return tweet_world2
+
 if __name__ == '__main__':
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_KEY_SECRET)
+    auth = tweepy.OAuthHandler('3Gkn2Y145WwZ5kFt4NBCBKVGm', 'nio5smQQLOex29tIEJiDWlXUiE8JCi8Y6fmuzQ1DOYKt2QCKrK')
+    auth.set_access_token('1355161653961228291-qHLAUQO5YNckIeln2qGn5bVK5T43AK', 'ZXVBIVxGHDLuKYJ0zMA9vbIsfmRporIqRw4790KMujNLN')
 
     # Create API object
     api = tweepy.API(auth)
@@ -52,6 +61,9 @@ if __name__ == '__main__':
         print('Error while authenticating API')
         sys.exit(1)
 
-    tweet = total_covid()
+    tweet = news_covid()
+    tweet2 = vaccine_covid()
     api.update_status(tweet)
+    time.sleep(35)
+    api.update_status(tweet2)
     print('Tweet successful')
